12bee948e1986d84418ab79e96c514346db01c78
antlogist <antlogist@gmail.com>
2022-05-05 22:58:48 +0300

    JQuery added

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,3 +5,7 @@ mix.sass('resources/sass/app.scss', 'app/assets/components/common/css/all.css');
 mix.js([
     'resources/js/app.js',
 ], 'app/assets/components/common/js/all.js');
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.min.js',
+], 'app/assets/libs/js/libs.js');
