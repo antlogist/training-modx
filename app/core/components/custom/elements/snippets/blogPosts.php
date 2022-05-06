1fb89687bb8c13a671cd8b6004ac12be24acaa39
antlogist <antlogist@gmail.com>
2022-05-06 11:53:53 +0300

    Comments added

--- app/core/components/custom/elements/snippets/blogPosts.php
+++ app/core/components/custom/elements/snippets/blogPosts.php
@@ -1,4 +1,6 @@
 <?php
+
+// DB request
 $posts = $modx->newQuery('modResource');
 $posts->where(array(
     'parent:IN' => array(6),
@@ -8,8 +10,11 @@ $posts->where(array(
 ));
 $posts->sortby('menuindex', 'ASC');
 $posts->limit(10);
+
+// Get collection 
 $resources = $modx->getCollection('modResource', $posts);
 
+// Main loop
 foreach($resources as $key => $value) {
 
     $wrapper = renderWrapper();
@@ -28,10 +33,7 @@ foreach($resources as $key => $value) {
     echo $wrapper['end'];
 }
 
-function renderDate($date) {
-    echo  '<small>' . date('d/m/Y', $date) . '</small>';
-}
-
+// Wrapper rendering
 function renderWrapper() {
     return array(
         'start'  => '<div class="my-5">',
@@ -39,22 +41,29 @@ function renderWrapper() {
     );
 }
 
+// Date rendering
+function renderDate($date) {
+    echo  '<small>' . date('d/m/Y', $date) . '</small>';
+}
+
+// Image rendering
 function renderImage($url, $alt) {
-    if(!$url) {
-        return;
-    }
+    if(!$url) { return; }
 
     echo '<img src="' . $url . '" alt="' . $alt . '" class="img-fluid">';
 }
 
+// Title rendering
 function renderTitle($title) {
     echo '<h2 class="mb-0 pb-0">'. $title .'</h2>';
 }
 
+// Short description rendering
 function renderIntro($intro) {
     echo '<div class="mt-1 mb-3">' . $intro . '</div>';
 }
 
+// Read more button rendering
 function renderReadMore($link = '#', $text = 'Читать') {
     echo '<a href="' . $link . '" class="btn btn-warning btn-sm">' . $text . '</a>';
 }
